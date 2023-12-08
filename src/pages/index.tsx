6e48c7c8ce5486d0a46afc152dee8adebff81a05
shivamonside <shivam.dubey@skilzen.com>
2023-12-08 22:41:32 +0530

    added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      
-        <h1>Welcome</h1>
-    
+      <h1>Welcome to my home</h1>
     </main>
-  )
+  );
 }
